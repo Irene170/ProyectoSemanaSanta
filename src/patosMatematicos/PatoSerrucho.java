5c516c771ce7215dd035c73d65c06311ac1c2424
irene <irenegonro2@gmail.com>
2024-04-01 12:41:26 +0200

    Añadido JavaDoc. Terminado proyecto.

--- src/patosMatematicos/PatoSerrucho.java
+++ src/patosMatematicos/PatoSerrucho.java
@@ -1,10 +1,25 @@
 package patosMatematicos;
 
+/** Esta clase es otro tipo de pato, hace sonidos, es desconfiado y resuelve ecuaciones cuadráticas
+* 
+* @author irene 
+*/
+/**
+ * @version 1.0
+ */
+
 public class PatoSerrucho {
+
 	String nombre;
 	String color;
 	int desconfianza;
 
+	/**
+	 * 
+	 * @param nombre       El nombre del PatoSerrucho
+	 * @param color        El color del PatoSerrucho
+	 * @param desconfianza El nivel de desconfianza del PatoSerrucho
+	 */
 	public PatoSerrucho(String nombre, String color, int desconfianza) {
 		super();
 		this.nombre = nombre;
@@ -12,49 +27,83 @@ public class PatoSerrucho {
 		this.desconfianza = desconfianza;
 	}
 
+	/**
+	 * 
+	 * @return Deevuelve el nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * 
+	 * @param nombre Establece el nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * 
+	 * @return Devuelve el color
+	 */
 	public String getColor() {
 		return color;
 	}
 
+	/**
+	 * 
+	 * @param color Establece el color
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
 
+	/**
+	 * 
+	 * @return Devuelve el nivel de desconfianza
+	 */
 	public int getDesconfianza() {
 		return desconfianza;
 	}
 
+	/**
+	 * 
+	 * @param desconfianza Etablece el nivel de desconfianza
+	 */
 	public void setDesconfianza(int desconfianza) {
 		this.desconfianza = desconfianza;
 	}
 
 	@Override
+	/** Permite ver todos los datos en String de la clase PatoSerrucho */
 	public String toString() {
 		return "PatoSerrucho [nombre=" + nombre + ", color=" + color + ", desconfianza=" + desconfianza + "]";
 	}
 
-	// Método para hacer que el pato haga sonidos característicos
+	/** Método para hacer que el pato haga sonidos característicos */
 	public void hacerSonido() {
 		System.out.println(nombre + " hace el sonido serrucho serrucho...");
 	}
-	// Método para resolver ecuaciones de segundo grado por patos
+	
+	
+
+	/** Método para resolver ecuaciones de segundo grado por patos */
+	/**
+	 * 
+	 * @param a Parámetro de la cuadrática
+	 * @param b Paramétro de la lineal
+	 * @param c Paramétro del término sin x 
+	 */
 
 	public void resolverEcuacionSegundoGrado(double a, double b, double c) {
 		// Calculamos el discriminante (lo que va dentro de la raíz)
 		double discriminante = b * b - 4 * a * c;
 
-		// Verificamos si hay soluciones reales, solo pueden calcular estas
-		// (las ecuciones cuadradas tienen dos soluciones, sean reales o no)
+		/** Verificamos si hay soluciones reales, solo pueden calcular estas
+		 (las ecuciones cuadradas tienen dos soluciones, sean reales o no)*/
 		if (discriminante > 0) {
-			// Dos soluciones reales distintas
+			/**Dos soluciones reales distintas*/
 
 			double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
 			double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
@@ -62,17 +111,17 @@ public class PatoSerrucho {
 					+ ", x2 = " + x2);
 			System.out.println("Me voy a nadar adiooss");
 		} else if (discriminante == 0) {
-			// Una solución real (raíz doble)
+			/** Una solución real (raíz doble)*/
 			double x = -b / (2 * a);
 			System.out.println("La solución doble es: x = " + x);
 			System.out.println("Soy " + nombre + "no me hagas trabajar más, este patito quiere nadaar");
 		} else {
-			// No hay soluciones reales
+			/** No hay soluciones reales*/
 			System.out.println("No hay soluciones reales. Este pato gruñón no lo puede resolver");
 		}
 	}
 
-	// Saber si es un pato muy desconfiado o no
+	/** Saber si es un pato muy desconfiado o no(Valorado del 1-10)*/
 	public void desconfiado() {
 
 		if (desconfianza <= 10) {
--- src/patosMatematicos/PatoZambullidor.java
+++ src/patosMatematicos/PatoZambullidor.java
@@ -5,7 +5,13 @@ public class PatoZambullidor {
 	int edad;
 	String color;
 	int salud;
-
+/**
+ * 
+ * @param nombre Es el nombre del PatoZambullidor
+ * @param edad Es la edad del Pato zambullidor
+ * @param color Es el color del Pato Zambullidor
+ * @param salud Es el "nivel" de salud del Pato Zambullidor
+ */
 	public PatoZambullidor(String nombre, int edad, String color, int salud) {
 		super();
 		this.nombre = nombre;
@@ -13,46 +19,83 @@ public class PatoZambullidor {
 		this.color = color;
 		this.salud = salud;
 	}
-
+/**
+ * 
+ * @return 
+ * Devuelve el nombre
+ */
 	public String getNombre() {
 		return nombre;
 	}
-
+/**
+ * 
+ * @param nombre 
+ * Declara el nombre
+ */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+/**
+ * 
+ * @return 
+ * Devuelve la edad 
+ */
 	public int getEdad() {
 		return edad;
 	}
-
+/**
+ * 
+ * @param edad 
+ * Declara la edad
+ */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
+/**
+ *  Devuelve el color 
+ * @return
+ */
 	public String getColor() {
 		return color;
 	}
-
+/**
+ * 
+ * @param color
+ * Declara el color
+ */
 	public void setColor(String color) {
 		this.color = color;
 	}
-
+/**
+ * 
+ * @return
+ * Devuelve el nivel de salud
+ */
 	public int getSalud() {
 		return salud;
 	}
-
+/**
+ * 
+ * @param salud
+ * Declara el nivel de salud 
+ */
 	public void setSalud(int salud) {
 		this.salud = salud;
 	}
 
 	@Override
+	/**
+	 * Devuelve una cadena de caracteres de los atributos de la clase Pato Zambllidor 
+	 */
 	public String toString() {
 		return "PatoZambullidor [nombre=" + nombre + ", edad=" + edad + ", color=" + color + ", salud=" + salud + "]";
 	}
-	// Método para resolver una ecuación lineal de primer grado de la forma "ax + b=
-	// 0"
-
+/** 
+ * Método para resolver una ecuación lineal de primer grado de la forma "ax + b= 0"
+ * 
+ * @param a Es el parámetro delante de la x
+ * @param b Es el parámetro delante de la y
+ */
 	public void resolverEcuacionLineal(int a, int b) {
 		if (a == 0) {
 			System.out.println("No es una ecuación lineal de primer grado. Este patito no lo puede resolver");
@@ -62,12 +105,12 @@ public class PatoZambullidor {
 		System.out.println(nombre + " ha resuelto la ecuación " + a + "x + " + b + " = 0 y x = " + x);
 	}
 
-	// Método para hacer que el pato haga quack
+	/** Método para hacer que el pato haga quack*/
 	public void quack() {
 		System.out.println(nombre + " hace quack quack!");
 	}
 
-	// Método para hacer que el pato nade
+	/** Método para hacer que el pato nade*/
 	public void nadar() {
 		System.out.println("Soy" + nombre + " ,estoy nadando en el estanqueee");
 		if (salud > 5) {
