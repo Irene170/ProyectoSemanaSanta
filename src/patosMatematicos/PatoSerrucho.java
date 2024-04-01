6512e9637312bbc81ea6220dcd3024ed2740ef9a
irene <irenegonro2@gmail.com>
2024-04-01 12:14:38 +0200

    Añado atributos y constructor

--- src/patosMatematicos/PatoSerrucho.java
+++ src/patosMatematicos/PatoSerrucho.java
@@ -1,5 +1,15 @@
 package patosMatematicos;
 
 public class PatoSerrucho {
+	String nombre;
+	String color;
+	int desconfianza;
+
+	public PatoSerrucho(String nombre, String color, int desconfianza) {
+		super();
+		this.nombre = nombre;
+		this.color = color;
+		this.desconfianza = desconfianza;
+	}
 
 }
--- src/patosMatematicos/PatoZambullidor.java
+++ src/patosMatematicos/PatoZambullidor.java
@@ -1,5 +1,17 @@
 package patosMatematicos;
 
 public class PatoZambullidor {
+	String nombre;
+	int edad;
+	String color;
+	int salud;
+
+	public PatoZambullidor(String nombre, int edad, String color, int salud) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.color = color;
+		this.salud = salud;
+	}
 
 }
